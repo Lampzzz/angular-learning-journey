b8a0aa89cd355747b42dfa7c1f5bae6df7a45011
Lampz <lampazaj@gmail.com>
2025-09-22 08:18:54 +0800

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
+import { UserProfile } from './playground/user-profile/user-profile';
+import { Form } from './playground/form/form';
+import { Card } from './playground/card/card';
 
 @Component({
   selector: 'app-root',
-  template: ` <div><h1>HELLO WORLD</h1></div> `,
-  imports: [],
+  // templateUrl: './app.html',
+  template: '<app-card/>',
+  styleUrl: './app.css',
+  imports: [UserProfile, Form, Card],
 })
-export class App {}
+export class App {
+  count: WritableSignal<number> = signal(2);
+  doubleCount = computed(() => {
+    console.log('Computing...');
+    return this.count() * 2;
+  });
+
+  reset() {
+    this.count.set(0);
+  }
+  increament() {
+    this.count.update((value) => value + 1);
+  }
+  decreament() {
+    this.count.update((value) => value - 1);
+  }
+}
---
+++ src/app/playground/form/form.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  imports: [],
+  standalone: true,
+  templateUrl: './form.html',
+  styleUrl: './form.css',
+})
+export class Form {}
