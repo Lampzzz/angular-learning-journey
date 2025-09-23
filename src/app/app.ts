30508c3465dc7f65a2b249e1f970cc4a82a4c21a
Lampz <lampazaj@gmail.com>
2025-09-23 20:54:15 +0800

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,16 @@
-import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
-import { UserProfile } from './playground/user-profile/user-profile';
-import { Form } from './playground/form/form';
+import { Component } from '@angular/core';
 import { Card } from './playground/card/card';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.html',
-  template: '<app-card/>',
+  templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [UserProfile, Form, Card],
+  imports: [Card],
 })
 export class App {
-  count: WritableSignal<number> = signal(2);
-  doubleCount = computed(() => {
-    console.log('Computing...');
-    return this.count() * 2;
-  });
+  childMessage = ""
 
-  reset() {
-    this.count.set(0);
-  }
-  increament() {
-    this.count.update((value) => value + 1);
-  }
-  decreament() {
-    this.count.update((value) => value - 1);
+  onMessageClick(message: string) {
+    this.childMessage = message
   }
 }
